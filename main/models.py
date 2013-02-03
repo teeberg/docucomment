f144f7684db97660404c23549483f491411d51a0
Nicola Marcacci Rossi <nicolamr@gmail.com>
2013-02-03 17:44:47 +0100

    Wikilinks to other documents

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import re
 
 # Create your models here.
 class Document(models.Model):
@@ -14,3 +15,20 @@ class Comment(models.Model):
 	document = models.ForeignKey(Document)
 	creation_date = models.DateTimeField('date created')
 	page = models.IntegerField()
+	def comment_parsed(self):
+		res = self.comment
+		link_regex = re.compile(r"\[\[([^\]/]+)(\/(\d)+)?\]\]")
+		def make_ahref(match):
+			pdf, pagepart, page = match.groups()
+			ds = Document.objects.filter(name=pdf)
+			if len(ds) >= 1:
+				d = ds[0]
+				if page == None:
+					return '<a href="/document/%s">%s</a>' % (d.hash, d.name)
+				else:
+					return '<a href="/document/%s?page=%s">%s (page %s)</a>' % (d.hash, page, d.name, page)
+			return match.group(0)
+		res = link_regex.sub(make_ahref, res)
+		#res = link_regex.sub("bla", res)
+		return res
+
--- main/views.py
+++ main/views.py
@@ -59,7 +59,7 @@ def comments(request, hash, page):
 	comments = Comment.objects.filter(document=d, page=page)
 	cs = []
 	for comment in comments:
-		c = {"nickname": comment.nickname, "comment": comment.comment}
+		c = {"nickname": comment.nickname, "comment": comment.comment_parsed()}
 		cs.append(c)
 	return HttpResponse(simplejson.dumps(cs))
 
