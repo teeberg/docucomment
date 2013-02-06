9785874d79811e8ed1e11ac111882491dbd3b5e6
Jonas Teeberg <teeberg@gmx.de>
2013-02-06 17:03:14 +0100

    Allow navigating to lexicographically previous and next documents

--- main/views.py
+++ main/views.py
@@ -53,10 +53,19 @@ def document(request, hash):
 	except:
 		return redirect("/document/"+hash)
 	initial = {}
+	d = ds[0]
+
+	# previous and next documents, for alphabetical navigation
+	try:    previous = Document.objects.filter(name__lt=d.name, public=1).order_by('-name')[0]
+	except: previous = None
+
+	try:    next     = Document.objects.filter(name__gt=d.name, public=1).order_by('name')[0]
+	except: next     = None
+
 	if "nickname" in request.COOKIES:
 		initial["nickname"] = request.COOKIES["nickname"]
 	form = CommentForm(initial=initial)
-	return render_to_response('main/document.html', {"document": ds[0], 'commentForm': form, 'page': page})
+	return render_to_response('main/document.html', {"document": d, 'commentForm': form, 'page': page, 'previous': previous, 'next': next})
 
 def comments(request, hash=None, page=None):
 	if hash == None:
@@ -73,16 +82,17 @@ def comments(request, hash=None, page=None):
 		cs.append(c)
 		if comment.document.public:
 			c["document_hash"] = comment.document.hash
-	return HttpResponse(simplejson.dumps(cs))
+	return JsonResponse(cs)
 
 def renamedocument(request, hash):
 	ret = {'status': 1}
-	d = Document.objects.get(hash=hash)
-	if d == None:
+	ds = Document.objects.filter(hash=hash)
+	if len(ds) == 0:
 		ret['message'] = 'No document exists with this hash.'
 	elif 'name' not in request.POST:
 		ret['message'] = 'No new name supplied.'
 	else:
+		d = ds[0]
 		try:
 			d.name = request.POST['name']
 			d.save()
@@ -91,7 +101,7 @@ def renamedocument(request, hash):
 		except Exception as ex:
 			ret['message'] = 'An exception was thrown. Please notify an administrator.'
 			traceback.print_exc(file=sys.stdout)
-	return HttpResponse(simplejson.dumps(ret))
+	return JsonResponse(ret)
 
 def setnickname(request):
 	response = {}
@@ -99,26 +109,33 @@ def setnickname(request):
 		nickname = strip_tags(request.POST['name'])
 		response['status'] = 0
 		response['safenick'] = nickname
-		http = HttpResponse(simplejson.dumps(response))
+		http = JsonResponse(response)
 		http.set_cookie("nickname", nickname)
 		return http
 	else:
 		response['status'] = 1
 		response['message'] = 'No new nick provided.'
-		return HttpResponse(simplejson.dumps(response))
+		return JsonResponse(response)
 
 def comment(request, hash, page):
 	if request.method == 'POST':
 		post = request.POST.copy()
 		post.update({'nickname': strip_tags(request.POST['nickname'])})
-		d = Document.objects.get(hash=hash)
-		if d == None:
-			return HttpResponse(simplejson.dumps({"status": 1, "message": "There is no document with this hash."}))
+		ds = Document.objects.filter(hash=hash)
+		if len(ds) == 0:
+			return JsonResponse({"status": 1, "message": "There is no document with this hash."})
+		d = ds[0]
 		if post.has_key('id') and len(post['id']) > 0:
-			commentForm = CommentForm(post, instance=Comment.objects.get(pk=int(post['id'])))
+			instance = Comment.objects.filter(pk=int(post['id']))
+			if len(instance) == 0:
+				return JsonResponse({"status": 1, "message": "Trying to update non-existing comment."})
+			elif instance.deleted == True:
+				return JsonResponse({"status": 1, "message": "This comment has been deleted."})
+
+			commentForm = CommentForm(post, instance=instance[0])
 			if (commentForm.is_valid()):
 				commentForm.save()
-				return HttpResponse(simplejson.dumps({"status": 0}));
+				return JsonResponse({"status": 0})
 		else:
 			commentForm = CommentForm(post)
 			if (commentForm.is_valid()):
@@ -127,7 +144,7 @@ def comment(request, hash, page):
 				c.document = d
 				c.page = page
 				c.save()
-				response = HttpResponse(simplejson.dumps({"status": 0, "safenick": c.nickname}))
+				response = JsonResponse({"status": 0, "safenick": c.nickname})
 				response.set_cookie("nickname", c.nickname)
 				return response
 	raise Http404
@@ -137,15 +154,26 @@ def login(request):
 
 def deletecomment(request, hash, id):
 	ds = Document.objects.filter(hash=hash)
-	if (len(ds) == 0):
-		raise Http404
-	d = ds[0]
-	c = Comment.objects.get(pk=id)
-	if (c.document.id != d.id):
-		raise HttpResponse(simplejson.dumps({"status": "error", "message": "Comment doesn't belong to document"}))
-	c.deleted = True
-	c.save()
-	return HttpResponse(simplejson.dumps({"status": "ok"}));
+	ret = {"status": 0}
+	if len(ds) == 0:
+		ret.update(status=1, message="No document with this ID exists")
+	else:
+		d = ds[0]
+		cs = Comment.objects.filter(pk=id)
+		if len(cs) == 0:
+			ret.update(status=1, message="No comment with this ID exists")
+		elif c.document.id != d.id:
+			ret.update(status=1, message="Comment does not belong to the provided document")
+		else:
+			c.deleted = True
+			c.save()
+	return JsonResponse(ret)
+
+class JsonResponse(HttpResponse):
+	def __init__(self, data, *args, **kwargs):
+		if 'content_type' not in kwargs:
+			kwargs.update(content_type='application/json')
+		super(JsonResponse, self).__init__(simplejson.dumps(data), *args, **kwargs)
 
 class CommentForm(forms.ModelForm):
 	class Meta:
