b789d0ee63e390e15ab650680bb6ac3ad28860e0
Jonas Teeberg <teeberg@gmx.de>
2013-03-02 14:16:17 +0100

    Remove the default commandline again, the user can set it up himself

--- manage.py
+++ manage.py
@@ -6,10 +6,4 @@ if __name__ == "__main__":
 	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "docucomment.settings")
 
 	from django.core.management import execute_from_command_line
-
-	# default command line if no argument given
-	commandline = "runserver 0.0.0.0:8000"
-	if len(sys.argv) == 1:
-		sys.argv.extend(commandline.split(' '))
-
 	execute_from_command_line(sys.argv)
